241d7fc3df932a897b528ac1d59dfddc9d94518d
Evan Rubinton <evanrubinton@gmail.com>
2014-09-18 20:30:50 -0400

    Add title editing to card edit modal

--- app/assets/javascripts/views/cards/cards_edit.js
+++ app/assets/javascripts/views/cards/cards_edit.js
@@ -1,15 +1,23 @@
 TrelloClone.Views.CardsEdit = Backbone.View.extend({
   template: JST['cards/edit_modal'],
   
+  tagName: 'div',
+  
+  className: 'edit-card-el',
+  
   initialize: function(){
   },
   
   events: {
-    'click .hide-modal': 'hideModal' 
+    'click' : 'checkHideModal',
+    'click .hide-modal': 'hideModal' ,
+    'click #edit-card-title-btn': 'editTitle',
+    'click .cancel-edit-button' : 'cancelEdit',
+    'submit #edit-card-title-form' : 'saveTitle'
   },
   
   render: function(){
-    var content = this.template({card: this.model});
+    var content = this.template({card: this.model, list: this.list});
     this.$el.html(content).appendTo('#modal-container');
     return this;
   },
@@ -18,8 +26,8 @@ TrelloClone.Views.CardsEdit = Backbone.View.extend({
     this.$el.empty();
     this.model = card;
     this.collection = cardCollection;
+    this.list = this.collection.list;
     this.render();
-    debugger
     this.$el.find('#modal').addClass("is-active");
   },
   
@@ -27,5 +35,29 @@ TrelloClone.Views.CardsEdit = Backbone.View.extend({
     event.preventDefault();
     event.stopPropagation();
     this.$el.find('#modal').removeClass("is-active");
+  },
+  
+  checkHideModal: function(event){
+    if(event.target.id == "modal"){
+      event.preventDefault();
+      this.$el.find('#modal').removeClass("is-active");
+    }
+  },
+  
+  editTitle: function(){
+    this.$el.toggleClass("edit-title");
+  },
+  
+  saveTitle: function(event){
+    event.preventDefault();
+    var attrs = $(event.target).serializeJSON();
+    this.model.save(attrs);
+    this.$el.find('#edit-card-title-btn').html(attrs.title);
+    this.cancelEdit();
+  },
+  
+  cancelEdit: function(){
+    this.$el.find('input[name=title]').val(this.model.get("title"));
+    this.$el.removeClass("edit-title");
   }
 });
